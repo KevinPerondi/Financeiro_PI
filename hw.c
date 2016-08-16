50377e31586911c479169683640e51e5cc14098b
Kevin Perondi Regis <kevinpregis@hotmail.com>
2016-08-16 19:13:38 -0300

    Update hw.c

--- hw.c
+++ hw.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main(void){
-	printf("E NOIX FEIX ISSU AKI");
+	int a,b;
+	printf("peso,altura");
+	scanf("%d,%d",&a,&b);
+	int c = a/(b*b);
+	printf("IMC: %d",c);
 return 0;
 }
