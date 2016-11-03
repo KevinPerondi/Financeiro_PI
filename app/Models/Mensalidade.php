5a822b5129009c7594538921f6d34c1308a6d087
KevinPerondi <kevinpregis@hotmail.com>
2016-11-03 20:11:26 -0200

    tabela mensalidades e model mensalidades

---
+++ app/Models/Mensalidade.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PI;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mensalidade extends Model
+{
+    //
+}
---
+++ database/migrations/2016_11_03_215835_create_mensalidades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMensalidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensalidades', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_membro');
+            $table->string('nome_membro');
+            $table->float('valor');
+            $table->string('vencimento');
+            $table->string('status');
+            $table->timestamps();
+        }); //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
