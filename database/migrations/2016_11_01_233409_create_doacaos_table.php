d0a58d2c698693082d11aa04a2e0fd29c78b7c84
felipe <felipeminorelli@gmail.com>
2016-11-03 16:25:07 -0200

    Ajuste Seeder doação

--- database/migrations/2016_11_01_233409_create_doacaos_table.php
+++ database/migrations/2016_11_01_233409_create_doacaos_table.php
@@ -1,33 +1,34 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateDoacaosTable extends Migration
 {
-
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('doacaos', function(Blueprint $table) {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('doacaos', function(Blueprint $table) {
             $table->increments('id');
-
+            $table->float('valor');
+            $table->string('donatario');
+            $table->string('data');
             $table->timestamps();
-		});
-	}
+        }); //
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('doacaos');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::drop('doacaos'); //
+    }
 }
